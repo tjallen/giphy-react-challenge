0a7afa7d64674d7e1257c98be06e69ab06cec3dc
Thom Allen <thomwork@gmail.com>
2018-03-22 10:17:30 +0000

    Fix gif title typo; add onGifClick prop

--- src/Gif.js
+++ src/Gif.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Gif = ({ id, src, title }) => (
-  <li><img src={src} alt="title" /></li>
+const Gif = ({ id, src, title, onGifClick }) => (
+  <li><img src={src} onClick={onGifClick} alt={title} /></li>
 );
 
 export default Gif;
