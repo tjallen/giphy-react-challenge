bc89ad9b7237ded160756926657a9172361ad06d
Thom Allen <thomwork@gmail.com>
2018-03-22 08:41:46 +0000

    Test initial Giphy API call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  query = '123'
+  giphyKey = 'uy4jAUKFBMvmVsq0YwWUBdCwGtB6X5kX'
+  limit = 10
+  offset = 0
+  componentDidMount() {
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${this.query}&api_key=${this.giphyKey}&limit=${this.limit}&offset=${this.offset}`)
+      .then(res => res.json())
+      .then(res => console.log(res))
+  }
   render() {
     return (
       <div className="App">
